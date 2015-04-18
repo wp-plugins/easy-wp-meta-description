b5b7e9f79d99abc3553fd159c06f57a1e5e51759
matswes <matswes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-18 08:49:27 +0000

    Adding Easy WP Meta Description 1.0
    git-svn-id: https://plugins.svn.wordpress.org/easy-wp-meta-description/trunk@1137845 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ easy-wp-meta-description.php
@@ -0,0 +1,122 @@
+<?php
+/*
+Plugin Name: Easy WP Meta Description
+Plugin URI: http://easy-wp-plugin.com/meta-description/
+Description: Adds meta description to each post
+Version: 1.0.0
+Author: Mats Westholm
+Author URI: http://matswestholm.se/
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Domain Path: /languages
+Text Domain: easy-wp-meta-description
+*/
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+$Easy_WP_Meta_Description = new Easy_WP_Meta_Description();
+$Easy_WP_Meta_Description->run();
+
+class Easy_WP_Meta_Description{
+
+	private $plugin_name;
+	private $meta_key;
+	
+	function __construct(){
+		$this->plugin_name = 'easy-wp-meta-description';
+		$this->meta_key = '_easy_wp_meta_description';
+	}
+	
+	function run(){
+		if( is_admin() ){
+			add_action( 'init', array( $this, 'load_plugin_textdomain' ) );
+			add_action( 'add_meta_boxes', array( $this, 'description_meta_box') );
+			add_action( 'save_post', array( $this, 'save_description' ) );
+		}
+		else{
+			add_action( 'wp_head', array( $this, 'insert_meta_in_head' ) );
+		}
+	}
+
+	function load_plugin_textdomain(){
+		load_plugin_textdomain( $this->plugin_name, false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+	}
+
+	function insert_meta_in_head(){
+		$description = '';
+		if( is_single() or is_page() ){
+			global $post;
+			$description = get_post_meta( $post->ID, $this->meta_key, true );
+//			print "Content: $content<br>Post id: $post->ID";
+		}
+		elseif( is_tag() or is_category() or is_tax()){
+			$remove = array( '<p>', '</p>' );
+			$description = trim( str_replace( $remove, '', term_description() ) );
+		}
+		elseif( is_front_page() ){
+			$description = get_bloginfo( 'description', 'display' );
+		}
+		if( $description ){
+				?>
+<meta name="description" content="<?php print $description; ?>">
+				<?php
+		}
+	}
+
+	function description_meta_box(){
+		$id = 'add_description';
+		$title =  'Easy WP Meta Description';
+		$callback = array( $this, 'add_description_meta_box' );
+		$context = 'normal';
+		$priority = 'high';
+		$callback_args = '';
+		
+		// get custom posttypes
+		$args = array( 'public'   => true, '_builtin' => false );
+		$output = 'names';
+		$operator = 'and';
+		$custom_posttypes = get_post_types( $args, $output, $operator );
+		
+		$builtin_posttypes = array( 'post', 'page' );
+		$screens = array_merge( $builtin_posttypes, $custom_posttypes );
+		foreach ( $screens as $screen ) {
+			add_meta_box( $id, $title, $callback, $screen, $context,
+				 $priority, $callback_args );
+		}
+	}
+
+	function add_description_meta_box(){
+		wp_nonce_field( 'add_description_meta_box', 'add_description_meta_box_nonce' );
+		$post_id = get_the_ID();
+		$value = get_post_meta( $post_id, $this->meta_key, true );?>
+<div class="wp-editor-container">
+<textarea class="wp-editor-area" name="add_description" cols="80" rows="5"><?php print $value; ?></textarea>
+</div>
+<p><?php _e( 'Add a meta description to your HTML code', $this->plugin_name ); ?></p>
+		<?php
+	}
+	
+	function save_description( $post_id ){
+		if( ! isset( $_POST['add_description_meta_box_nonce'] ) ){
+			return;
+		}
+		if( ! wp_verify_nonce( $_POST['add_description_meta_box_nonce'], 'add_description_meta_box' ) ){
+			return;
+		}
+		if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+			return;
+		}
+		if( ! isset( $_POST['add_description'] ) ){
+			return;
+		}
+		$data = sanitize_text_field(	$_POST['add_description'] );
+		update_post_meta( $post_id, $this->meta_key, $data );
+	}	
+
+} // class
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,30 @@
+=== Easy WP Meta Description ===
+Contributors: Mats Westholm
+Donate link: http://easy-wp-plugin.com/donation/
+Tags: meta, description, header, head
+Requires at least: 3.1
+Tested up to: 4.1.1
+Stable tag: 1.0
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Simple and easy to use wordpress pluging to add the meta description tag in html head
+
+== Description ==
+This plugin lets the user write a seperate description for each post, page or custom post type post which is added as a meta description to the html head. 
+
+It also adds meta description to category, tag and custom taxonomy archive as well as front page. For category, tag and custom taxonomy the Description you can edit on the Edit Category or Edit Tag pages in the Dashboard is used. For front page the Tagline under Settings > General Settings is used.
+
+== Installation ==
+Put the unzipped files in the plugin directory and go to Dashboard > Plugins to activate.
+
+== Frequently Asked Questions ==
+Questions are answered in the forum and FAQ will be posted at http://easy-wp-plugin.com/faq/
+
+== Screenshots ==
+
+1. Type your description
+
+== Changelog ==
+
+No changes yet
